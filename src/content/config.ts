b9fe68c79a778afe5a834dcc00c0ad0bcb0df0ac
Cassandra Melax <cassandra@melax.com>
2026-01-01 23:34:58 -0800

    Make writeup description/date optional

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,8 +3,8 @@ import { defineCollection, z } from 'astro:content';
 const writeups = defineCollection({
 	schema: z.object({
 		title: z.string(),
-		description: z.string(),
-		date: z.date(),
+		description: z.string().optional().default(''),
+		date: z.date().optional(),
 		status: z.enum(['draft', 'published']).default('published'),
 	}),
 });
